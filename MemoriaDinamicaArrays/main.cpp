96f5d2c737f7893e1c87c23796a7d3b370d06760
J0aq14007 <joaquinarenasriveros@gmail.com>
2025-10-23 23:20:27 -0500

    Punteros y orden

--- MemoriaDinamicaArrays/main.cpp
+++ MemoriaDinamicaArrays/main.cpp
@@ -32,7 +32,7 @@ void swap(int *a, int *b){
 
 }
 
-int bubble(int *ptr, int tam){
+void bubble(int *ptr, int tam){
 
     for(int i=0; i<tam-1; i++){
         for(int j=0; j<tam-i-1; j++){
@@ -44,8 +44,53 @@ int bubble(int *ptr, int tam){
 }
 /**
 insertion, merge, quick
-
 **/
+
+void insertion(int *ptr, int tam){
+
+    for(int j = 1; j < tam; j++){ 
+         int key = ptr[j];
+         int i;
+         
+         for(int i= j-1; i>0 && ptr[i] > key; i--){
+             ptr[i+1] = ptr[i];
+         }
+         ptr[i+1] = key;
+    }
+
+}
+
+void quick(int *ptr, int tam){
+     
+     int i = 0;
+     while (i < tam - 1) {
+        int pivote = ptr[tam - 1]; 
+        int j = i;
+        int k = tam - 2;
+
+        while (j <= k) {
+        
+            if (ptr[j] > pivote && ptr[k] < pivote){
+            
+                swap(&ptr[j+1], &ptr[k-1]);
+            }
+            else if (ptr[j] <= pivote){
+                j++;
+            }
+            else if (ptr[k] >= pivote){
+                k--;
+            }
+        }
+        swap(&ptr[j], &ptr[tam - 1]); 
+        tam--; 
+    }
+
+}
+
+void merge(int *ptr, int tam){
+
+     
+}
 int main()
 {
     int *ptr = new int[5];
@@ -64,11 +109,35 @@ int main()
 
 
     bubble(ptr, tam);
-
+    cout<<"BUBBLE"<<endl;
     for(int i=0; i<5; i++){
         cout<<ptr[i]<<" ";
     }
-
+    cout<<endl;
+    
+    for(int i=0; i<5; i++){
+        ptr[i] = tam-i;
+    }
+    
+    insertion(ptr, tam);
+     cout<<"INSERTION"<<endl;
+    for(int i=0; i<5; i++){
+        cout<<ptr[i]<<" ";
+    }
+    cout<<endl;
+    
+    for(int i=0; i<5; i++){
+        ptr[i] = tam-i;
+    }
+    
+    quick(ptr,tam);
+    cout<<"QUICK"<<endl;
+    for(int i=0; i<5; i++){
+        cout<<ptr[i]<<" ";
+    }
+    cout<<endl;
+  
     delete[] ptr;
+    
     return 0;
 }
